1f80426cf8bfc8850ac7c328b85b2a44db46f7a4
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-08-17 09:16:48 +0300

    feat: menu

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+const url = window.location.pathname;
+const editedUrl = url.replace('/', '');
+
+const link = document.querySelector(`.nav__link[href="${editedUrl}"]`);
+
+link.classList.add('nav__link--selected');
